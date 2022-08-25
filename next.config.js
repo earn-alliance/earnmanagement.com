bee198935cea38af486fe89ea4d6e82dec71340d
Gorav Garg <abhijeets.rathor@thewitslab.com>
2022-08-25 18:28:30 +0530

    layout landing page

--- next.config.js
+++ next.config.js
@@ -1,7 +1,26 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-};
+const nextEnv = require('next-env');
+const dotenvLoad = require('dotenv-load');
+const withImages = require('next-images');
+dotenvLoad();
+withImages();
+const withNextEnv = nextEnv();
+const ImageGetLink = 'http://localhost:3000';
+module.exports = withNextEnv({
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    domains: ['localhost', 'storage.googleapis.com'],
+    loader: 'imgix',
+    path: ImageGetLink,
+    trailingSlash: true,
+  },
+  presets: ['next/babel'],
 
-module.exports = nextConfig;
+  async exportPathMap() {
+    return {
+      '/': { page: '/' },
+      '/home': { page: '/home' },
+    };
+  },
+});
