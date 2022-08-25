a9280cdda7e54cf9f6dafca472ff3d82948fdd6a
Gorav Garg <abhijeets.rathor@thewitslab.com>
2022-08-25 17:06:37 +0530

    Dependency added

--- next.config.js
+++ next.config.js
@@ -1,17 +1,28 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  exportPathMap: async function (
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
+const nextEnv = require('next-env');
+const dotenvLoad = require('dotenv-load');
+const withImages = require('next-images');
+dotenvLoad();
+withImages();
+const withNextEnv = nextEnv();
+const ImageGetLink =
+  process.env.NEXT_PUBLIC_IMAGE_PATH || 'https://www.earnalliance.com/';
+module.exports = withNextEnv({
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    domains: ['localhost', 'storage.googleapis.com'],
+    loader: 'imgix',
+    path: ImageGetLink,
+    trailingSlash: true,
+  },
+  presets: ['next/babel'],
+
+  async exportPathMap() {
     return {
       '/': { page: '/' },
       '/home': { page: '/home' },
       '/index': { page: '/index' },
     };
   },
-};
-
-module.exports = nextConfig;
+});
